9f67c25a3395e8ea5ef1b572d901dd718450f944
Naveen1st <naveenbanoth4889@gmail.com>
2023-02-11 22:17:04 +0530

    first commit

---
+++ src/main/java/com/driver/Boat.java
@@ -0,0 +1,20 @@
+package com.driver;
+
+public class Boat implements WaterVehicle{
+
+    private String name;
+    private int capacity;
+
+    public Boat(){
+
+    }
+    @Override
+    public String getVehicleName() {
+        return this.name;
+    }
+
+    @Override
+    public int getVehicleCapacity() {
+        return this.capacity;
+    }
+}
